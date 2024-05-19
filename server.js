029c873f4c3663479bb595346030d9140fb9d1ae
K Harsh <harshnaidu2u@gmail.com>
2024-05-19 23:23:37 +0530

    user route, cookieparser, globalErrorHandler

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express")
 require("dotenv").config()
+const cookieParser = require("cookie-parser")
 
 const globalErrorHanlder = require("./controllers/errorController")
 const AppError = require("./utils/appError")
 const taskRoutes = require("./routes/taskRoutes")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express()
 const PORT = 5000
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/taskapi/v1', taskRoutes)
+app.use('/taskapi/v1', userRoutes)
 
 // HANDLING UNHANDLED ROUTES
 app.all('*', (req, res, next) => {
